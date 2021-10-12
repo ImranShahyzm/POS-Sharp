396b40ac3e14267539ec6a83926257e9351eb8ce
Corbis Soft <Corbis Soft@DESKTOP-Q54D9J1>
2021-10-12 15:26:09 +0500

    Commit

--- POS/DbScripts/Script 1.0.0.67.sql
+++ POS/DbScripts/Script 1.0.0.67.sql
@@ -139,3 +139,42 @@ Insert into data_CashOut(
 
 
 GO
+
+
+/****** Object:  Table [dbo].[PosData_ShiftRecords]    Script Date: 10/12/2021 3:25:40 PM ******/
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[PosData_ShiftRecords]') AND type in (N'U'))
+DROP TABLE [dbo].[PosData_ShiftRecords]
+GO
+/****** Object:  Table [dbo].[PosData_ShiftRecords]    Script Date: 10/12/2021 3:25:40 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[PosData_ShiftRecords]') AND type in (N'U'))
+BEGIN
+CREATE TABLE [dbo].[PosData_ShiftRecords](
+	[ShiftID] [int] IDENTITY(1,1) NOT NULL,
+	[ShiftName] [nvarchar](100) NULL,
+	[StartTime] [datetime] NULL,
+	[ISCuurentlyRunning] [bit] NULL,
+	[LastExecuted] [datetime] NULL,
+	[CompanyID] [int] NULL,
+	[WHID] [int] NULL,
+ CONSTRAINT [PK_PosData_ShiftRecords] PRIMARY KEY CLUSTERED 
+(
+	[ShiftID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+END
+GO
+SET IDENTITY_INSERT [dbo].[PosData_ShiftRecords] ON 
+
+GO
+INSERT [dbo].[PosData_ShiftRecords] ([ShiftID], [ShiftName], [StartTime], [ISCuurentlyRunning], [LastExecuted], [CompanyID], [WHID]) VALUES (1, N'A', CAST(N'2021-01-01 12:00:00.000' AS DateTime), 0, NULL, 1, 8)
+GO
+INSERT [dbo].[PosData_ShiftRecords] ([ShiftID], [ShiftName], [StartTime], [ISCuurentlyRunning], [LastExecuted], [CompanyID], [WHID]) VALUES (2, N'B', CAST(N'2021-01-01 08:00:00.000' AS DateTime), 0, NULL, 1, 8)
+GO
+INSERT [dbo].[PosData_ShiftRecords] ([ShiftID], [ShiftName], [StartTime], [ISCuurentlyRunning], [LastExecuted], [CompanyID], [WHID]) VALUES (3, N'C', CAST(N'2021-01-01 11:59:00.000' AS DateTime), 0, NULL, 1, 8)
+GO
+SET IDENTITY_INSERT [dbo].[PosData_ShiftRecords] OFF
+GO
