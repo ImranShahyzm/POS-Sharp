8ac3da6c74ce47c767f9ba4434453756da23f492
Ahsan <Corbis Soft@DESKTOP-Q54D9J1>
2022-02-11 18:49:40 +0500

    Commit

--- POS/DbScripts/Script 1.0.0.77.sql
+++ POS/DbScripts/Script 1.0.0.77.sql
@@ -59,3 +59,44 @@ where data_SalePosReturnInfo.SalePosID=@SaleInvoice
 
 
 GO
+
+
+-- Server : DESKTOP-Q54D9J1 -- 
+
+alter procedure [dbo].[rpt_Fbr_sale_invoice] 
+@SaleInvoice bigint=0 
+as
+select CategoryName,InventItems.ItemNumber,InventWareHouse.PhoneNo,InventWareHouse.LocationAddress,InventWareHouse.WHDesc,
+InventItems.ItenName,InventItems.UrduName,data_SalePosInfo.customerName,data_SalePosInfo.CustomerPhone,
+data_SalePosInfo.SalePosNo as SalePosID,data_SalePosInfo.SalePosDate,data_SalePosInfo.TaxAmount, data_SalePosInfo.GrossAmount, data_SalePosDetail.DiscountPercentage, data_SalePosInfo.DiscountAmount, 
+data_SalePosInfo.DiscountTotal, data_SalePosInfo.OtherCharges, data_SalePosInfo.NetAmount, data_SalePosInfo.AmountReceive, 
+data_SalePosInfo.AmountReturn, data_SalePosInfo.SalePosReturnID, data_SalePosInfo.AmountInAccount, data_SalePosInfo.AmountReceivable, 
+data_SalePosInfo.AmountPayable,GLCompany.*,
+data_SalePosDetail.ItemId,data_SalePosDetail.Quantity,data_SalePosDetail.ItemRate,data_SalePosDetail.TotalAmount,data_SalePosDetail.DiscountAmount as DetailDiscount,FBR_InvoiceMaster.InvoiceNumber,FBR_InvoiceMaster.imagePath,
+gen_PosConfiguration.POSID,gen_PosConfiguration.BillPreFix,
+data_SalePosDetail.TaxPercentage,data_SalePosDetail.TaxAmount as DtTaxAmount
+from data_SalePosInfo left join
+FBR_InvoiceMaster on FBR_InvoiceMaster.SalePOSID=data_SalePosInfo.SalePosID
+inner join data_SalePosDetail on data_SalePosInfo.SalePosID=data_SalePosDetail.SalePosID
+inner join InventItems on data_SalePosDetail.ItemId=InventItems.ItemId
+inner join GLCompany on GLCompany.Companyid=data_SalePosInfo.CompanyID
+inner join InventWareHouse on InventWareHouse.WHID=data_SalePosInfo.WHID
+left join InventCategory on InventCategory.CategoryID=InventItems.CategoryID
+inner join gen_PosConfiguration on gen_PosConfiguration.CounterID=data_SalePosInfo.CounterID
+where data_SalePosInfo.SalePosID=@SaleInvoice
+
+
+Go
+
+
+
+
+
+
+
+
+
+
+
+
+
