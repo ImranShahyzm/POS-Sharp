a8428021fbc2e221a95a5df773d1deb664a2a88d
Ahsan <Corbis Soft@DESKTOP-Q54D9J1>
2022-07-13 14:49:10 +0500

    c

---
+++ POS/DbScripts/Script 1.0.0.91.sql
@@ -0,0 +1,413 @@
+﻿
+/****** Object:  StoredProcedure [dbo].[rpt_saleReturn_invoice]    Script Date: 7/13/2022 2:48:01 PM ******/
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[rpt_saleReturn_invoice]') AND type in (N'P', N'PC'))
+DROP PROCEDURE [dbo].[rpt_saleReturn_invoice]
+GO
+/****** Object:  StoredProcedure [dbo].[data_SalePosReturnInfo_Insert]    Script Date: 7/13/2022 2:48:01 PM ******/
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[data_SalePosReturnInfo_Insert]') AND type in (N'P', N'PC'))
+DROP PROCEDURE [dbo].[data_SalePosReturnInfo_Insert]
+GO
+/****** Object:  StoredProcedure [dbo].[data_SalePosReturnInfo_Insert]    Script Date: 7/13/2022 2:48:01 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[data_SalePosReturnInfo_Insert]') AND type in (N'P', N'PC'))
+BEGIN
+EXEC dbo.sp_executesql @statement = N'CREATE PROCEDURE [dbo].[data_SalePosReturnInfo_Insert] AS' 
+END
+GO
+
+
+
+
+
+
+
+
+
+
+
+
+
+ALTER procedure [dbo].[data_SalePosReturnInfo_Insert] 
+@UserID int = null , 
+@CompanyID int = null , 
+@FiscalID int = null ,
+@SalePosDate	date	=null,
+@WHID	int	=null,
+@TaxAmount	decimal(18, 2)	=null,
+@GrossAmount	decimal(18, 2)	=null,
+@DiscountPercentage	decimal(18, 2)	=null,
+@DiscountAmount	decimal(18, 2)	=null,
+@DiscountTotal	decimal(18, 2)	=null,
+@OtherCharges	decimal(18, 2)	=null,
+@NetAmount	decimal(18, 2)	=null,
+@AmountReceive	decimal(18, 2)	=null,
+@AmountReturn	decimal(18, 2)	=null,
+@AmountInAccount	decimal(18, 2)=null,
+@AmountReceivable	decimal(18, 2)	=null,
+@AmountPayable	decimal(18, 2)	=null,
+@data_SalePosDetail  data_SalePosDetail   readonly,
+@SalePosID bigint output,
+@CustomerPhone nvarchar(50)=null,
+@CustomerName  nvarchar(50)=null,
+@SaleInvoiceNo int=0,
+@MenuId int=0,
+@InvoiceType int=0,
+@CustomerID int=null,
+@RiderAmount numeric(18,3)=0,
+@LinckedBill int =NULL,
+@SaleManId int =NULL,
+@CounterID int=NULL,
+@SalePosReturnID int=0 output
+as
+declare @returnSale bit=0
+declare @IsTaxable bit =0
+--declare @SalePosReturnID bigint=0
+declare @SalePosReturnDetailID bigint=0
+declare @SalePosDetailID bigint=0
+declare @ISOfflineServer bit=0,@StylePOS nvarchar(50)
+
+Select @StylePOS=POSStyle from gen_posConfiguration where CounterID=@CounterID
+
+IF @StylePOS='POSPcWorldStyle'
+BEGIN
+Select  @ISOfflineServer=ISNULL(IsofflineServer,0) from gen_posConfiguration where CounterID=@CounterID
+END
+ELSE 
+BEGIN
+Select  @ISOfflineServer=ISNULL(IsofflineServer,0) from gen_posConfiguration where WHID=@WHID
+END
+declare @MaxTable as MaxTable 
+declare @MaxTable2 as MaxTable
+
+declare  @SaleID bigint
+set @SaleID=@SalePosID
+--set @SalePOSReturnID=1
+
+declare @WhereClause1 nvarchar(4000),@WhereClauseDetail1 nvarchar(4000),@ReturnSaleID bigint
+set @WhereClause1=' '
+select @SalePosID=SalePosID from data_SalePosInfo where saleposNo=@SaleInvoiceNo and SalePosDate=@SalePosDate
+--if(@ReturnSaleID is null)
+--	begin 
+--		set @ReturnSaleID=0
+--	end
+--	if @ReturnSaleID=0 
+--	begin
+--	set @WhereClause1='select * from data_SalePosDetail where SalePosID='+cast(@SalePosID as nvarchar(10));
+--	end
+--	else
+--	begin
+--		set @WhereClause1='select * from data_SalePosReturnDetail where IsLog=0 and SalePosReturnID='+cast(@ReturnSaleID as nvarchar(10));
+--	end
+
+declare @IsLog bit
+select @IsLog=IsLogActive from gen_systemConfiguration where CompanyID=@CompanyID
+
+
+if(@SalePosID>0)
+begin
+	insert into @MaxTable
+exec GetVoucherNoPosFoodMama @Fieldname=N'SalePOSNo',@TableName=N'data_SalePosReturnInfo',@CheckTaxable= 0 ,@PrimaryKeyValue = @SalePosReturnID,@PrimaryKeyFieldName = 'SalePosReturnID' ,
+	@voucherDate=@SalePOSDate,@voucherDateFieldName=N'SalePosReturnDate',@companyID=@CompanyID,@companyFieldName=N'CompanyID',@FiscalID=@FiscalID, 
+	@FiscalIDFieldName=N'FiscalID',@IsTaxable=@IsTaxable,@MenuId=@MenuId,@MenuFieldName=N'MenuId',@CounterID=@CounterID,@CounterField=N'CounterID'
+	select @SaleInvoiceNo = VoucherNo from @MaxTable
+
+--This Will Record Sale Replace or Sale Edit
+
+Insert into data_SalePosReturnInfo (
+		SalePosID, 
+		EntryUserID,
+		EntryUserDateTime,
+		CompanyID,
+		FiscalID,
+		SalePosReturnDate,
+		WHID,
+		TaxAmount,
+		GrossAmount,
+		DiscountPercentage,
+		DiscountAmount,
+		DiscountTotal,
+		OtherCharges,
+		NetAmount,
+		AmountReceive,
+		AmountReturn,
+		AmountInAccount,
+		AmountReceivable,
+		AmountPayable,
+		CustomerPhone,
+		CustomerName ,
+		SalePOSNo,
+		MenuId,
+		InvoiceType,
+		SaleManId,
+		CounterID
+	)
+	Select 
+	SalePosID, 
+	EntryUserID,
+	GETDATE(),
+	CompanyID,
+	@FiscalID,
+	@SalePosDate,
+	@WHID,
+	@TaxAmount,
+	@GrossAmount,
+	@DiscountPercentage,
+	@DiscountAmount,
+	@DiscountTotal,
+	@OtherCharges,
+	@NetAmount,
+	@AmountReceive,
+	@AmountReturn,
+	@AmountInAccount,
+	@AmountReceivable,
+	@AmountPayable,
+	@CustomerPhone,
+	@CustomerName ,
+	@SaleInvoiceNo,
+	@MenuId,
+	@InvoiceType,
+	@SaleManId,
+	@CounterID
+
+	from data_SalePosInfo
+	where SalePosID=@SalePosID
+	set @SalePosReturnID = SCOPE_IDENTITY()
+	
+
+	Insert Into data_SalePosReturnDetail (
+		SalePosReturnID,
+		SalePosDetailID,
+		ItemId,
+		Quantity,
+		ItemRate,
+		TaxPercentage,
+		TaxAmount,
+		DiscountPercentage,
+		DiscountAmount,
+		TotalAmount,
+		CartonSize,
+		Carton,
+		TotalQuantity,
+		SchemeID,
+		MinQuantity , 
+		IMEINumber
+		) Select
+		@SalePosReturnID ,
+		SalePosDetailID,
+		ItemId,
+		Quantity,
+		ItemRate,
+		TaxPercentage,
+		TaxAmount,
+		DiscountPercentage,
+		DiscountAmount,
+		TotalAmount,
+	
+		CartonSize,
+		Carton,
+		TotalQuantity,
+		SchemeID,
+		MinQuantity, 
+		IMEINumber
+
+		From @data_SalePosDetail 
+		
+end
+
+
+
+
+
+		 declare @productInflowID int
+DECLARE @PractitionerId int,@StockRate numeric(18,3)=0
+declare @PosSaleReturnDetailID int ,@PosStockRate numeric(14,5) , @PosQuantity numeric(10,2),@PosItemId int,@PosstockQty numeric(14,2),@ManufacturingID int=0, @RIMEINumber nvarchar(1000), @RIMEINumber1 nvarchar(1000), @RIMEINumber2 nvarchar(1000), @RIsBatchItem bit=0 , @Rdata_ItemBatchDetail  data_ItemBatchDetail
+	
+DECLARE MY_CURSOR CURSOR 
+  LOCAL STATIC READ_ONLY FORWARD_ONLY
+FOR 
+SELECT SalePosReturnDetailID from data_SalePosReturnInfo inner join data_SalePosReturnDetail 
+on data_SalePosReturnInfo.SalePosReturnID=data_SalePosReturnDetail.SalePosReturnID 
+where data_SalePosReturnDetail.IsLog=0 and  data_SalePosReturnInfo.SalePosID=@SalePosID
+
+OPEN MY_CURSOR
+FETCH NEXT FROM MY_CURSOR INTO @PractitionerId
+WHILE @@FETCH_STATUS = 0
+BEGIN 
+    --Do something with Id here
+    PRINT @PractitionerId
+	SELECT @PosSaleReturnDetailID=SalePosReturnDetailID,@PosItemId=ItemId,@PosStockRate=ItemRate,@PosQuantity=Quantity,
+	@SalePosDetailID=SalePosDetailID , @RIMEINumber=IMEINumber
+from data_SalePosReturnInfo inner join data_SalePosReturnDetail 
+on data_SalePosReturnInfo.SalePosReturnID=data_SalePosReturnDetail.SalePosReturnID 
+where data_SalePosReturnDetail.IsLog=0 and SalePosReturnDetailID=@PractitionerId
+
+Select @RIsBatchItem=IsBatchItem from InventItems where ItemId=@PosItemId
+
+Select @ManufacturingID=ManaufacturingID from data_SalePosDetail where SalePosDetailID=@SalePosDetailID
+
+SET @RIMEINumber1 = NULL; SET @RIMEINumber2 = NULL;
+
+IF @RIMEINumber IS NOT NULL AND @RIMEINumber <> ''
+BEGIN
+	IF CHARINDEX(',', @RIMEINumber) > 0
+	BEGIN
+		SET @RIMEINumber1 = Substring(
+							@RIMEINumber, 
+							1, 
+							Charindex(',', @RIMEINumber)-1
+							);
+		SET @RIMEINumber2 = Substring(
+							@RIMEINumber, 
+							Charindex(',', @RIMEINumber)+1, 
+							LEN(@RIMEINumber)-Charindex(',', @RIMEINumber)
+							);
+	END
+	ELSE
+	BEGIN
+		SET @RIMEINumber1 = @RIMEINumber;
+	END
+		
+END
+	
+IF @RIsBatchItem = 1 AND @RIMEINumber1 IS NOT NULL
+BEGIN
+	INSERT INTO @Rdata_ItemBatchDetail (RowID, Quantity, Param1, Param2, Param3)
+	SELECT @PractitionerId , @PosQuantity, @RIMEINumber1, @RIMEINumber2, NULL
+END
+
+	--watch this
+	exec data_ProductInflow_Insert @productInflowID out,@PosItemId,@PosSaleReturnDetailID,'POS RETURN',@WHID, @SalePosDate , 
+				@PosStockRate,@PosQuantity,@PosQuantity,@FiscalID , @CompanyID ,@IsTaxable, NULL, NULL, NULL, NULL, NULL, NULL, NULL
+				, @PractitionerId , @Rdata_ItemBatchDetail
+				exec tempInflowDataTable_Insert @productInflowID , 'POS RETURN'
+	--watht this end
+	update data_SalePosReturnDetail set IsLog=1 where SalePosReturnDetailID=@PosSaleReturnDetailID
+
+
+	exec GetWeightedRateForItem @PosItemId, @WHID ,@SalePosDate ,@SalePosDate ,@StockRate output , @CompanyID , @IsTaxable
+update data_SalePosReturnDetail set StockRate=@StockRate where SalePosReturnDetailID=@PosSaleReturnDetailID
+
+
+
+if(@ManufacturingID>0)
+begin
+exec data_ManufacturingInfo_delete @ManufacturingID=@ManufacturingID
+end
+    FETCH NEXT FROM MY_CURSOR INTO @PractitionerId
+END
+CLOSE MY_CURSOR
+DEALLOCATE MY_CURSOR
+
+
+
+if @ISOfflineServer=1
+begin
+
+exec  data_SalePOSReturnInfo_InsertAccountOffline @CounterID=@CounterID,@SalePosReturnID=@SalePosReturnID, @SalePosID = @SalePosID, @CompanyID = @CompanyID, @SalePosDate = @SalePosDate , @WHID = @WHID , 
+@NetAmount = @NetAmount , @SaleInvoiceNo = @SaleInvoiceNo , @UserID = @UserID , @FiscalID = @FiscalID ,@EditMode = 0,
+ @AmountReceivable = @NetAmount,@data_SalePosDetail=@data_SalePosDetail
+
+end
+
+
+
+
+		DECLARE @Amount decimal(18,2)=0 
+		Select @AmountPayable=@NetAmount
+		select @Amount=(select isnull(sum(Amount),0) as Amount from data_CashIn where Date=cast(GETDATE() as date))
+                                  -
+         (select isnull(sum(Amount),0) as Amount from data_CashOut where Date=cast(GETDATE() as date)) 
+		 --select @Amount
+		declare @ErrorMessage nvarchar(1000)
+		if @AmountPayable>0
+		begin
+			if	@Amount>=@AmountPayable
+			begin
+				EXEC [dbo].[data_Cash_Insert]
+				@SourceID = @SalePosID,
+				@SourceName = N'POS Return',
+				@Amount = @AmountPayable,
+				@CashType = 0,
+				@SalePosDate=@SalePosDate,
+				@CounterID=@CounterID
+			end
+			else
+				begin
+				set @ErrorMessage = 'Not have enough cash!'
+					RAISERROR (@ErrorMessage , 16, 1 ) ;
+					return
+				end
+		end
+		else
+		begin
+			EXEC [dbo].[data_Cash_Insert]
+			@SourceID = @SalePosID,
+			@SourceName = N'POS Replace',
+			@Amount = @AmountReceivable,
+			@CashType = 1,
+			@CounterID=@CounterID
+		end
+		
+
+
+
+
+
+
+
+
+
+
+
+select @SalePosID
+
+
+
+
+GO
+/****** Object:  StoredProcedure [dbo].[rpt_saleReturn_invoice]    Script Date: 7/13/2022 2:48:01 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[rpt_saleReturn_invoice]') AND type in (N'P', N'PC'))
+BEGIN
+EXEC dbo.sp_executesql @statement = N'CREATE PROCEDURE [dbo].[rpt_saleReturn_invoice] AS' 
+END
+GO
+
+
+
+
+
+ALTER procedure [dbo].[rpt_saleReturn_invoice] 
+@SaleInvoice bigint=0 
+as
+select CategoryName,InventItems.ItemNumber,InventWareHouse.PhoneNo,InventWareHouse.LocationAddress,InventWareHouse.WHDesc,
+InventItems.ItenName,InventItems.UrduName,data_SalePosReturnInfo.customerName,data_SalePosReturnInfo.CustomerPhone,
+data_SalePosReturnInfo.SalePosNo as SalePosID,data_SalePosReturnInfo.SalePosReturnDate as SalePOSDate,data_SalePosReturnInfo.TaxAmount, data_SalePosReturnInfo.GrossAmount, data_SalePosReturnDetail.DiscountPercentage, data_SalePosReturnInfo.DiscountAmount, 
+data_SalePosReturnInfo.DiscountTotal, data_SalePosReturnInfo.OtherCharges, data_SalePosReturnInfo.NetAmount, data_SalePosReturnInfo.AmountReceive, 
+data_SalePosReturnInfo.AmountReturn, data_SalePosReturnInfo.SalePosReturnID, data_SalePosReturnInfo.AmountInAccount, data_SalePosReturnInfo.AmountReceivable, 
+data_SalePosReturnInfo.AmountPayable,GLCompany.*,
+data_SalePosReturnDetail.ItemId,data_SalePosReturnDetail.Quantity,data_SalePosReturnDetail.ItemRate,data_SalePosReturnDetail.TotalAmount,data_SalePosReturnDetail.DiscountAmount as DetailDiscount,
+dbo.data_SalePosReturnDetail.IMEINumber
+from data_SalePosReturnInfo 
+inner join data_SalePosReturnDetail on data_SalePosReturnInfo.SalePosReturnID=data_SalePosReturnDetail.SalePosReturnID
+inner join InventItems on data_SalePosReturnDetail.ItemId=InventItems.ItemId
+inner join GLCompany on GLCompany.Companyid=data_SalePosReturnInfo.CompanyID
+inner join InventWareHouse on InventWareHouse.WHID=data_SalePosReturnInfo.WHID
+left join InventCategory on InventCategory.CategoryID=InventItems.CategoryID
+where data_SalePosReturnInfo.SalePosReturnID=@SaleInvoice
+
+
+
+
+
+
+
+
+
+GO
